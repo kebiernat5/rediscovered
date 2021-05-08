cebdef08e02c43d1bd9f399879f42a8da4eb1235
James V <james.vanhoke@yahoo.com>
2021-05-08 00:06:42 -0700

    Global state reduced

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -3,11 +3,25 @@ import React, { createContext, useReducer, useContext } from "react";
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
-const reducer = (state) => {return state}
+
+// Reducer to allow us to set our global state
+const reducer = (state, action) => {
+  switch (action.type) {
+  case "SetImages":
+    console.log("I'm the payload", action.payload)
+    return {
+      imageArray: action.payload,
+    };
+
+  default:
+    return state;
+  }
+};
+
 
 const StoreProvider = ({ value = [], ...props }) => {
   const defaultState = { }
-
+  
   const [state, dispatch] = useReducer(reducer, defaultState);
 
   return <Provider value={[state, dispatch]} {...props} />;
