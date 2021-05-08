d00bc5a91f87c39f80158819ffabfcc0af726ae8
James V <james.vanhoke@yahoo.com>
2021-05-08 00:06:17 -0700

    Totally no freebies in here

--- client/src/pages/Practice.js
+++ client/src/pages/Practice.js
@@ -6,6 +6,22 @@ import Card from "../components/Card";
 import { useStoreContext } from "../utils/GlobalState";
 
 function Practice() {
+
+// {props.employees.map(({ picture, name, phone, email, dob, id }) => {
+//   return (
+//     <Card
+//       key={id.value}
+//       image={picture.thumbnail}
+//       firstName={name.first}
+//       lastName={name.last}
+//       phone={phone}
+//       email={email}
+//       date={dob.date.slice(0, -14)}
+//     />
+//   );
+// })}
+
+
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const { user } = useAuth();
